59f87c7ad4d60e2b5d4f8c60f88d374ba6ede29d
Grigorii K. Shartsev <me@shgk.me>
2020-11-30 21:41:40 +0500

    Add 10-Architecture final

--- 10-Architecture/03-renderless-components/script.js
+++ 10-Architecture/03-renderless-components/script.js
@@ -1,16 +1,7 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js';
 
-const ListView = {
-  template: `<div>
-  <template v-for="(item, idx) in items_">
-    <slot name="default" :item="item">
-      <span>{{ item }}</span>
-    </slot>
-    <slot name="remove-button" :remove="() => remove(idx)">
-      <button @click="remove(idx)">x</button>
-    </slot>
-  </template>
-  </div>`,
+const RenderlessListView = {
+  // template: `<div><slot :items="items_" :methods="{ remove }" /></div>`,
 
   props: {
     items: Array,
@@ -18,8 +9,18 @@ const ListView = {
 
   data() {
     return {
-      items_: [...this.items],
+      items_: [],
     };
+  },
+
+  watch: {
+    items: {
+      deep: true,
+      immediate: true,
+      handler() {
+        this.items_ = [...this.items];
+      },
+    },
   },
 
   methods: {
@@ -28,22 +29,62 @@ const ListView = {
       this.$emit('update:items', [...this.items_]);
     },
   },
+
+  render(h) {
+    return this.$scopedSlots.default({
+      items: this.items_,
+      methods: {
+        remove: this.remove,
+      },
+    });
+  },
+};
+
+const UlListView = {
+  template: `<renderless-list-view :items="list" @update:items="$emit('update:list', $event)">
+    <template #default="{ items, methods }">
+      <ul>
+        <li v-for="(item, idx) in items">
+          <span>{{ item }}</span>
+          <button type="button" @click="methods.remove(idx)">Delete</button>
+        </li>
+      </ul>
+    </template>
+  </renderless-list-view>`,
+
+  components: { RenderlessListView },
+
+  props: {
+    list: Array,
+  },
+};
+
+const AListView = {
+  components: { RenderlessListView },
+
+  template: `<renderless-list-view :items="list" @update:items="$emit('update:list', $event)">
+    <template #default="{ items, methods }">
+      <div>
+        <a v-for="(item, index) in items" @click="methods.remove(index)" href="#">{{ item }}</a>
+      </div>
+    </template>
+  </renderless-list-view>`,
+
+  props: {
+    list: Array,
+  },
 };
 
 const App = {
   template: `<div>
-  <list-view :items.sync="list">
-    <template #default="scope">
-      <b>{{ scope.item }}</b>
-    </template>
-    <template #remove-button="{ remove }">
-      <a href="#" @click="remove">Remove</a>
-    </template>
-  </list-view>
+    <ul-list-view :list.sync="list"/>
+    <hr>
+    <a-list-view :list.sync="list" />
   </div>`,
 
   components: {
-    ListView,
+    UlListView,
+    AListView,
   },
 
   data() {
@@ -51,6 +92,6 @@ const App = {
       list: [1, 2, 3, 4, 5],
     };
   },
-}
+};
 
 const app = new Vue(App).$mount('#app');
